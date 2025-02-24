c901e4d6540b4ccc1c84116d489d689b996e4b51
Mac <nicola.macchioni01@gmail.com>
2025-02-24 21:47:24 +0100

    works

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 import pytz
 import yaml
 from tools.final_answer import FinalAnswerTool
+from tools.document_qa import DocumentQATool
 
 from Gradio_UI import GradioUI
 
@@ -40,6 +41,8 @@ web_search_tool = DuckDuckGoSearchTool()
 
 visit_webpage_tool = VisitWebpageTool()
 
+document = DocumentQATool()
+
 # If the agent does not answer, the model is overloaded, please use another model or the following Hugging Face Endpoint that also contains qwen2.5 coder:
 # model_id='https://pflgm2locj2t89co.us-east-1.aws.endpoints.huggingface.cloud' 
 
@@ -59,7 +62,7 @@ with open("prompts.yaml", 'r') as stream:
     
 agent = CodeAgent(
     model=model,
-    tools=[final_answer, web_search_tool, get_current_time_in_timezone, visit_webpage_tool], ## add your tools here (don't remove final answer)
+    tools=[final_answer, web_search_tool, get_current_time_in_timezone, visit_webpage_tool, document], ## add your tools here (don't remove final answer)
     max_steps=6,
     verbosity_level=1,
     grammar=None,
